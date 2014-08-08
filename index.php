d611612082b48311d9357377b56069571e55c7d6
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-08-08 14:41:56 -0500

    archivos iniciales

---
+++ index.php
@@ -0,0 +1,32 @@
+<!--https://www.youtube.com/watch?v=gBo7nQRLYAQ-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta chartset="utf-8">
+	<title>Selectores</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+			<li>6</li>
+			<li>7</li>
+			<li>8</li>
+			<li>9</li>
+			
+		</ul>
+	</div>
+
+		<ul>
+			<h2>Titulo</h2>
+			<li class="nav">Menu1</li>
+			<li class="nav">Menu2</li>
+			<li class="nav">Menu3</li>
+		</ul>
+</body>
+</html>
